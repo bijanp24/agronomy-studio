1d17b1996f56731a28d283cf456d2bc59ddfccf3
Bijan <bijan.pourazari@gmail.com>
2026-05-26 19:43:01 -0700

    build(app-config): wire HttpClient, router, and animations

---
+++ src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
+  },
+  {
+    path: 'fields',
+    loadComponent: () =>
+      import('./pages/fields/fields.component').then(m => m.FieldsComponent),
+  },
+  {
+    path: 'entropy',
+    loadComponent: () =>
+      import('./pages/entropy/entropy.component').then(m => m.EntropyComponent),
+  },
+  {
+    path: 'gis',
+    loadComponent: () =>
+      import('./pages/gis/gis.component').then(m => m.GisComponent),
+  },
+  { path: '**', redirectTo: 'dashboard' },
+];
